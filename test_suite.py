c38969d1a702f41f04e06c64f3c1314dc37a285b
nscherre <nscherre@usc.edu>
2024-10-01 19:05:55 -0700

    Finish Test Suite

--- test_suite.py
+++ test_suite.py
@@ -18,34 +18,39 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 # appropriate threshold levels and set them 
 # accordingly. Then, use them to determine
 # when it is light or dark, quiet or loud.
-lux_threshold=0  # change this value
+lux_threshold=300  # change this value
 sound_threshold=0 # change this value
 
 LUXPIN = 0
 SOUNDPIN = 1
 
-epoch = time.time() 
+for i in range(0,5): 
+  GPIO.output(pin, GPIO.HIGH)
+  time.sleep(.5)
+  GPIO.output(pin, GPIO.LOX)
+  time.sleep(.5)
 
-system_time = time.time() - epoch
-while True: 
-  if system_time <= 5: 
-    # Blink LED 
-    GPIO.output(pin, GPIO.HIGH)
-    time.sleep(.5) 
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(.5) 
-  elif system_time <= 10: 
-    # Read Light Sensor and Output Value #
-
-    val = mcp.read_adc(LUXPIN)
-    print(val)
-    if val > lux_threshold: 
-      print("BRIGHT")
-    else: 
-      print("DARK")
-
-    time.sleep(.1)
+for i in range(0,50):
+  light_val = mcp.read_adc(LUXPIN)
+  print(light_val)
+  if light_val > lux_threshold: 
+    print("BRIGHT")
   else: 
-    break
+    print("DARK")
+  time.sleep(.1)
 
-  system_time = time.time() - epoch
+for i in range(0,4): 
+  GPIO.output(pin, GPIO.HIGH)
+  time.sleep(.2)
+  GPIO.output(pin, GPIO.LOX)
+  time.sleep(.2)
+
+for i in range(0,50):
+  sound_val = mcp.read_adc(SOUNDPIN)
+  print(sound_val)
+  if sound_val > sound_threshold: 
+    GPIO.output(pin, GPIO.HIGH)
+  else: 
+    GPIO.output(pin, GPIO.HIGH)
+  
+  time.sleep(.1)
